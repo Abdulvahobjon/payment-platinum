6c4251cdffff4fed573c185ced4f0be8bb71fb4a
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-01-13 07:54:50 +0500

    tram and addition section add

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow:{
+        "card-shadow": "20px 4px 50px 0px rgba(0, 0, 0, 0.25)",
+        "card-shadow-2":"10px 4px 20px 0px rgba(0, 0, 0, 0.25);"
+      }
+    },
   },
   plugins: [],
 }
+
 
