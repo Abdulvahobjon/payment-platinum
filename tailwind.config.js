178833e05f6f0e3490178cdc00981bfcd4df3c63
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-01-13 08:19:39 +0500

    contact section the end

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     extend: {
       boxShadow:{
         "card-shadow": "20px 4px 50px 0px rgba(0, 0, 0, 0.25)",
-        "card-shadow-2":"10px 4px 20px 0px rgba(0, 0, 0, 0.25);"
+        "card-shadow-2":"10px 4px 20px 0px rgba(0, 0, 0, 0.25);",
+        "card-shadow-3":"10px 4px 15px 0px rgba(0, 0, 0, 0.25);"
       }
     },
   },
