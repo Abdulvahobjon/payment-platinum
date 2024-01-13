38c91032e78310d04dbb1157f2b3ca3806be848a
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-01-13 23:35:35 +0500

    commid

--- public/js/app.js
+++ public/js/app.js
@@ -27,3 +27,40 @@ close.addEventListener('click' , function(){
   document.body.classList.remove("overflow-hidden");
   document.body.classList.remove("h-screen");
 })
+
+document.querySelector('form').addEventListener('submit', function() {
+  // Formadagi malumotlarni olish
+  var email = document.querySelectorAll('input')[0].value;
+  var firstName = document.querySelectorAll('input')[1].value;
+  var lastName = document.querySelectorAll('input')[2].value;
+  var phoneNumber = document.querySelectorAll('input')[3].value;
+
+  // Malumotlarni JSON formatida tayyorlash
+  var data = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      phoneNumber: phoneNumber
+  };
+
+  // Fetch API orqali POST so'rov yuborish
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+      console.log('Yuborilgan malumotlar:', data);
+      // Boshqa harakatlar ham qilishingiz mumkin, masalan, xabar chiqarish
+      alert('Malumotlar muvaffaqiyatli yuborildi!');
+      document.querySelector('form').reset()
+  })
+  .catch(error => {
+      console.error('Xatolik:', error);
+      // Boshqa harakatlar ham qilishingiz mumkin, masalan, xabar chiqarish
+      alert('Malumotlarni yuborishda xatolik yuz berdi.');
+  });
+});
